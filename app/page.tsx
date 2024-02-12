14f0644a614240886d9a5a1a9cb5a6752d0e8779
D_Zucho <danny.nico.dev@outlook.com>
2024-02-12 11:42:26 -0800

    add initial component files and start navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
 
 export default function Home() {
-  return <h1 className='text-bold text-teal-500'>hello world</h1>
+  return <h1 className='font-bold text-teal-500 text-2xl'>hello world</h1>
 }
