99ae1e10cbbb33cc39eba818ca48796f11ec729f
D_Zucho <danny.nico.dev@outlook.com>
2024-02-19 18:26:19 -0800

    start footer

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,18 +3,27 @@ import Image from 'next/image'
 type ButtonProps = {
   type: 'button' | 'submit'
   title: string
-  variant: 'btn_dark_green' | 'btn_green' | 'btn_white_text'
+  variant:
+    | 'btn_dark_green'
+    | 'btn_green'
+    | 'btn_white_text'
+    | 'btn_white'
+    | 'btn_dark_green_outline'
   icon?: string
+  full?: boolean
 }
 
-const Button = ({ type, title, variant, icon }: ButtonProps) => {
+const Button = ({ type, title, variant, icon, full }: ButtonProps) => {
+  const fullClass = full ? 'w-full' : ''
   return (
     <button
       type={type}
-      className={`${variant} flexCenter gap-3 rounded-full border`}
+      className={`${variant} ${fullClass} {full ? &&'w-full'} flexCenter gap-3 rounded-full border`}
     >
       {icon && <Image src={icon} alt={title} width={24} height={24} />}
-      <label className='bold-16 whitespace-nowrap'>{title}</label>
+      <label className='bold-16 whitespace-nowrap cursor-pointer'>
+        {title}
+      </label>
     </button>
   )
 }
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,24 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <footer className='flexCenter mb-24'>
+      <div className='padding-container max-container w-full flex-col gap-14'>
+        <div className='flex flex-col items-start justify-center gap-[10%] md:flex-row'>
+          <Link href='/' className='mb-10'>
+            <Image
+              src={'/images/hilink-logo.svg'}
+              alt='logo'
+              width={74}
+              height={29}
+            />
+          </Link>
+        </div>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
